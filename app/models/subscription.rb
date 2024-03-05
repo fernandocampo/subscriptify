43aa020abbda51709f558ac850a213b231ebb813
Dario Cardoso <darioferreira10@gmail.com>
2024-03-05 10:47:51 -0300

    Validations update

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,4 +10,12 @@ class Subscription < ApplicationRecord
   validates :frequency, inclusion: { in: FREQUENCE }
   validates :category, inclusion: { in: CATEGORIES }
   validates :currency, inclusion: { in: CURRENCIES }
+
+  validates :company_name, presence: true
+  validates :recurrent, presence: true
+  validates :frequency, presence: true
+  validates :payment_date, presence: true
+  validates :category, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :currency, presence: true
 end
