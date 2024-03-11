7013c174c529313cbbb192724a5d2999bbc308fb
Fernando Campo <fernandocampow@gmail.com>
2024-03-11 18:37:43 -0300

    application js #

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,12 +6,12 @@ const application = Application.start()
 application.debug = false
 window.Stimulus = application
 
-// Import your controllers
-import ChangeClassController from "./change_class_controller"
-import PageRefreshController from "./page_refresh_controller"
+// // Import your controllers
+// import ChangeClassController from "./change_class_controller"
+// import PageRefreshController from "./page_refresh_controller"
 
-// Register your controllers
-application.register("change-class", ChangeClassController)
-application.register("page-refresh", PageRefreshController)
+// // Register your controllers
+// application.register("change-class", ChangeClassController)
+// application.register("page-refresh", PageRefreshController)
 
 export { application }
