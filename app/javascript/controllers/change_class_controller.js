230c9248f0333f1ff5cb52ed7116926392f4b560
antonio edwards <antonio.edwards1992@gmail.com>
2024-03-13 16:03:31 -0300

    home

--- app/javascript/controllers/change_class_controller.js
+++ app/javascript/controllers/change_class_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
-
 export default class extends Controller {
   static targets = ["name", "company", "payment", "category", "firstBtn" ]
-  connect() {
-  }
+
+  connect() { }
 
   nameEmpty() {
     if (this.nameTarget.value !== "") {
@@ -12,13 +11,23 @@ export default class extends Controller {
     }
   }
 
-  // Hacer la validacion de 1 y 2
   stepOne() {
     this.companyTarget.classList.add("d-none")
     this.paymentTarget.classList.remove("d-none")
   }
+
   stepTwo() {
     this.paymentTarget.classList.add("d-none")
     this.categoryTarget.classList.remove("d-none")
+  }
+
+  stepBack() { // Función dentro de la clase Controller
+    if (this.paymentTarget.classList.contains("d-none")) {
+      this.paymentTarget.classList.remove("d-none"); // Quitamos "add"
+      this.companyTarget.classList.remove("d-none");
+    } else if (this.categoryTarget.classList.contains("d-none")) {
+      this.categoryTarget.classList.remove("d-none"); // Quitamos "add"
+      this.paymentTarget.classList.remove("d-none");
+    }
   }
 }
