e06cb7ca65fc9f15fa74d842dc93de52aa6d1368
Fernando Campo <fernandocampow@gmail.com>
2024-03-13 15:47:50 -0300

    stimulus out

--- app/javascript/controllers/page_refresh_controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  // connect() {
-  //   history.replaceState({}, document.title, window.location.pathname);
-  // }
-}
