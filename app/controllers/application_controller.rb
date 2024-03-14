55cc9ceac82ad6d08697dc917d9ca95770f2a9fe
Dario Cardoso <darioferreira10@gmail.com>
2024-03-14 16:12:17 -0300

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :age, :city])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :age, :city, :photo])
   end
 
   def default_url_options
