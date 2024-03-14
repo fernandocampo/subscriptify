2c65ff38e91de93889c67badf10a652fad4b30e1
Dario Cardoso <darioferreira10@gmail.com>
2024-03-14 12:39:21 -0300

    para enrique

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
+  end
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
+
+  # validates :phone, presence: true
+  # validates :user, presence: true
+  # validates :name, presence: true
+  # validates :last_name, presence: true
+  # validates :age, presence: true
+  # validates :city, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
 
-  # validates :phone, presence: true
-  # validates :user, presence: true
-  # validates :name, presence: true
-  # validates :last_name, presence: true
-  # validates :age, presence: true
-  # validates :city, presence: true
   def due_dates
     subscriptions.map{ |subscription| subscription.payment_date.day }
   end
