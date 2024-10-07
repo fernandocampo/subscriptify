5b13f9a8b21f43a2bf1a2bc16c1fed40f1f250e1
Fernando Campo <fernandocampow@gmail.com>
2024-10-07 17:41:18 -0300

    Optimize Puma configuration for Heroku memory usage

--- config/puma.rb
+++ config/puma.rb
@@ -4,23 +4,24 @@
 
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# We reduced the maximum thread count to lower memory consumption.
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }  # Reduced to 3 threads
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
+# Specifies the worker count to be 2 in production to save memory.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })  # Set to 2 workers
   workers worker_count if worker_count > 1
 end
 
+# Preload the application to save memory with copy-on-write in production.
+preload_app!
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 30 if ENV.fetch("RAILS_ENV", "development") == "production"  # Set timeout for production
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
