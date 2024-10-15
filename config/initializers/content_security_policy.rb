c60d29a66104eb2313afb62a7d6911ac1ed6bd0b
Fernando Campo <fernandocampow@gmail.com>
2024-10-15 17:21:56 -0300

    security improvement

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,22 +4,26 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https
+    policy.style_src   :self, :https
+  end
+
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w(script-src style-src)
+end
+
+Rails.application.config.permissions_policy do |policy|
+  policy.camera :none
+  policy.geolocation :none
+  policy.microphone :none
+
+end
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
