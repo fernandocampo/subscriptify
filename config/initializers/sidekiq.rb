7a96e1fb238fca455cc7e085d00ae3b4ecf06a22
Fernando Campo <fernandocampow@gmail.com>
2024-10-17 22:00:06 -0300

    cron changed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,10 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+require 'sidekiq/cron/job'
 
+Sidekiq.configure_server do |config|
   config.on(:startup) do
     if File.exist?(File.expand_path('config/schedule.yml', Rails.root))
       schedule_file = File.expand_path('config/schedule.yml', Rails.root)
       Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
     end
   end
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
 end
