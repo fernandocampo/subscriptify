12e7bb04029876095eeb1adb1f3d52e675cf7409
Fernando Campo <fernandocampow@gmail.com>
2024-10-31 22:44:32 -0300

    Implement disposable email validation for user sign-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_03_14_134314) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_01_001439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -45,6 +45,13 @@ ActiveRecord::Schema[8.0].define(version: 2024_03_14_134314) do
   create_table "emails", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "nondisposable_disposable_domains", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_nondisposable_disposable_domains_on_name", unique: true
   end
 
   create_table "subscriptions", force: :cascade do |t|
