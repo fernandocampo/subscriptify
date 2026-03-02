ce7ff9d169c3c53c69b9e4c28bacdbee11424192
Fernando Campo <fernandocampow@gmail.com>
2026-03-02 15:22:20 -0300

    chore: cambiar queue adapter de sidekiq a async

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,6 @@ module Subscriptify
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     
-    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :async
   end
 end
