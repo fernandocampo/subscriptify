d5c4445c2f4d33f36ecd30a8b71cf35dd28b776d
Fernando Campo <fernandocampow@gmail.com>
2026-03-02 15:21:49 -0300

    fix: usar deliver_now en EmailerJob para ejecución directa via cron

--- app/jobs/emailer_job.rb
+++ app/jobs/emailer_job.rb
@@ -3,7 +3,7 @@ class EmailerJob
 
   def perform
     User.find_each do |user|
-      UserMailer.with(user: user).notification_helper.deliver_later
+      UserMailer.with(user: user).notification_helper.deliver_now
     end
   end
 end
